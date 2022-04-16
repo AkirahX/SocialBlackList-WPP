dc31cf59b9de038dacd5caf2ccab222f1344283c
AkirahX <akirah.ofc@gmail.com>
2022-04-15 22:57:36 -0300

    n

--- src/wpp.js
+++ src/wpp.js
@@ -12,41 +12,45 @@ setInterval(() => {
 const {state, saveState} = useSingleFileAuthState('./auth.json')
 
 const start = async () => {
-    const {version, isLasted} = await fetchLatestBaileysVersion()
+	try{
+		const {version, isLasted} = await fetchLatestBaileysVersion()
 
-    const wa = makeWASocket({
-        version,
-        logger: P({ level: 'silent' }),
-        printQRInTerminal: true,
-        auth: state,
-    })
-    store.bind(wa.ev)
-    //wa.ev.on('chats.set', item => console.log(`recv ${item.chats.length} chats (is latest: ${item.isLatest})`))
-	//wa.ev.on('messages.set', item => console.log(`recv ${item.messages.length} messages (is latest: ${item.isLatest})`))
-	//wa.ev.on('contacts.set', item => console.log(`recv ${item.contacts.length} contacts`))
-    //wa.ev.on('messages.upsert', async m => {})
-    //wa.ev.on('messages.update', m => console.log(m))
-	//wa.ev.on('message-receipt.update', m => console.log(m))
-	//wa.ev.on('presence.update', m => console.log(m))
-	//wa.ev.on('chats.update', m => console.log(m))
-	//wa.ev.on('contacts.upsert', m => console.log(m))
+		const wa = makeWASocket({
+			version,
+			logger: P({ level: 'silent' }),
+			printQRInTerminal: true,
+			auth: state,
+		})
+		store.bind(wa.ev)
+		//wa.ev.on('chats.set', item => console.log(`recv ${item.chats.length} chats (is latest: ${item.isLatest})`))
+		//wa.ev.on('messages.set', item => console.log(`recv ${item.messages.length} messages (is latest: ${item.isLatest})`))
+		//wa.ev.on('contacts.set', item => console.log(`recv ${item.contacts.length} contacts`))
+		//wa.ev.on('messages.upsert', async m => {})
+		//wa.ev.on('messages.update', m => console.log(m))
+		//wa.ev.on('message-receipt.update', m => console.log(m))
+		//wa.ev.on('presence.update', m => console.log(m))
+		//wa.ev.on('chats.update', m => console.log(m))
+		//wa.ev.on('contacts.upsert', m => console.log(m))
 
-    wa.ev.on('connection.update', (update) => {
-		const { connection, lastDisconnect } = update
-		if(connection === 'close') {
-			// reconnect if not logged out
-			if((lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut) {
-				start()
-			} else {
-				console.log('Connection closed. You are logged out.')
+		wa.ev.on('connection.update', (update) => {
+			const { connection, lastDisconnect } = update
+			if(connection === 'close') {
+				// reconnect if not logged out
+				if((lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut) {
+					start()
+				} else {
+					console.log('Connection closed. You are logged out.')
+				}
 			}
-		}
 
-		console.log('connection update', update)
-	})
-	// listen for when the auth credentials is updated
-	wa.ev.on('creds.update', saveState)
-    return wa
+			console.log('connection update', update)
+		})
+		// listen for when the auth credentials is updated
+		wa.ev.on('creds.update', saveState)
+		return wa
+	} catch (e){
+		console.log(e)
+	}
 }
 
 module.exports = start
