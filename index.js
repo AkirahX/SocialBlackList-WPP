e9f4ae529a3a4f5942d9267603ce3fff8929db40
AkirahX <akirah.ofc@gmail.com>
2022-05-09 01:44:27 -0300

    o

--- index.js
+++ index.js
@@ -6,23 +6,51 @@ const getStatus = require('./src/status')
 const {getMessageType, getText} = require('./src/messageVerify')
 const chalk = require('chalk')
 const {check} = require('./src/get')
+const { jidDecode } = require('@adiwajshing/baileys')
+const webHooks = require('node-webhooks')
+
+const decodeId = (id) => {
+    if(!id) return id
+    if(/:\d+@/gi.test(id)) {
+        let decode = jidDecode(id) || {}
+        return decode.user && decode.server && decode.user + '@' + decode.server || id
+    } else return id 
+}
+
+
+//create array of emojis erro and sucess
+const registerHooks = () => {
+    return new webHooks({
+        db: {
+            'callback_hook': ['https://eoe9g6kd2j9w7je.m.pipedream.net']
+        }
+    });
+}
 
 const init = async () => {
     global.wa = await start()
     wa.ev.on('group-participants.update', async (update) => {
+        console.log(decodeId(wa.user.id))
         try{
             let id = update.id
             let participants = update.participants
             let action = update.action
             console.log(chalk.blue('ID: ') + chalk.green(id) + chalk.blue(' Participants: ') + chalk.green(participants) + chalk.blue(' Action:') + chalk.green(action))
             if(action === 'add') {
+                if(participants.startsWith('92')){
+                    await wa.groupParticipantsUpdate(id, [participants], 'remove')
+                    console.log(chalk.red('Removed: ') + chalk.green(participants))
+                    await wa.sendMessage(id, `Sorry, you are not allowed to join this group.`)
+                    return
+                }
                 const chk = await check(participants[0])
                 if(chk.exists) {
                     let text = `_The user ${chk.whatsappId} is on the Social Black List_
 
-    _Description: ${chk.description}_`
+_Description: ${chk.description}_`
                     try{
                         await wa.groupParticipantsUpdate(id, [participants[0]], 'remove')
+                        console.log(chalk.red('Removed: ') + chalk.green(participants[0]))
                     } catch(error) {
                         await wa.sendMessage(id, {text: '_It was not possible to remove the user because this instance must be the administrator of the group._'})
                         console.log(error)
